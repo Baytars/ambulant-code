cf1b34c246e6ae2a0f2d79991f772c6154a1c4b3
Daniel Benden <koelimoe@sf.net>
2004-09-15 14:57:53 +0000

    more work on de factory struc, almost done

--- include/ambulant/common/player.h
+++ include/ambulant/common/player.h
@@ -53,6 +53,7 @@
 #ifndef AMBULANT_COMMON_PLAYER_H
 #define AMBULANT_COMMON_PLAYER_H
 
+#include "ambulant/common/factory.h"
 #include "ambulant/config/config.h"
 
 namespace ambulant {
@@ -95,7 +96,7 @@ class player {
 
 // Factory functions - should these be here?
 player *create_mms_player(lib::document *doc, window_factory *wf, playable_factory *rf);
-player *create_smil2_player(lib::document *doc, window_factory *wf, playable_factory *rf, common::embedder *sys);
+player *create_smil2_player(lib::document *doc, common::factories* factory, common::embedder *sys);
 
 } // namespace common
  
