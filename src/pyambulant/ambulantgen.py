7d2ebf4d89cf129c99e61deacea32a48b1c53032
Jack Jansen <Jack.Jansen@cwi.nl>
2005-06-14 11:45:20 +0000

    Checking in a snapshot: node.h parses but the resulting code doesn't compile yet.

--- src/pyambulant/ambulantgen.py
+++ src/pyambulant/ambulantgen.py
@@ -23,19 +23,8 @@ f = CxxMethodGenerator(const_node_interface_ptr, 'get_last_child',
 )
 methods_node_interface.append(f)
 
-f = CxxMethodGenerator(void, 'get_children',
-    (std_list<const_node_ptr>_ref, 'l', InMode),
-)
-methods_node_interface.append(f)
-
 f = CxxMethodGenerator(node_interface_ptr, 'locate_node',
-    (char_ptr, 'path', InMode),
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(void, 'find_nodes_with_name',
-    (xml_string_ref, 'name', InMode),
-    (std_list<node_interface_ptr>_ref, 'list', InMode),
+    (stringptr, 'path', InMode),
 )
 methods_node_interface.append(f)
 
@@ -57,36 +46,13 @@ f = CxxMethodGenerator(node_interface_ptr, 'clone',
 methods_node_interface.append(f)
 
 f = CxxMethodGenerator(void, 'append_data',
-    (char_ptr, 'data', InMode),
-    (size_t, 'len', InMode),
+    (InBuffer, 'data', InMode),
 )
 methods_node_interface.append(f)
 
 f = CxxMethodGenerator(void, 'set_attribute',
-    (char_ptr, 'name', InMode),
-    (char_ptr, 'value', InMode),
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(void, 'set_attributes',
-    (char_ptr_ptr, 'attrs', InMode),
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(void, 'set_namespace',
-    (xml_string_ref, 'ns', InMode),
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(const_xml_string_ref, 'get_namespace',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(const_xml_string_ref, 'get_local_name',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(const_q_name_pair_ref, 'get_qname',
+    (stringptr, 'name', InMode),
+    (stringptr, 'value', InMode),
 )
 methods_node_interface.append(f)
 
@@ -94,34 +60,12 @@ f = CxxMethodGenerator(int, 'get_numid',
 )
 methods_node_interface.append(f)
 
-f = CxxMethodGenerator(const_xml_string_ref, 'get_data',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(xml_string, 'get_trimmed_data',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(bool, 'has_graph_data',
-)
-methods_node_interface.append(f)
-
 f = CxxMethodGenerator(net_url, 'get_url',
-    (char_ptr, 'attrname', InMode),
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(const_q_attributes_list_ref, 'get_attrs',
+    (stringptr, 'attrname', InMode),
 )
 methods_node_interface.append(f)
 
 f = CxxMethodGenerator(unsigned_int, 'size',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(void, 'create_idmap',
-    (std_map<std, 'string', InMode),
-    (node_interface_ptr>_ref, 'm', InMode),
 )
 methods_node_interface.append(f)
 
@@ -130,18 +74,6 @@ f = CxxMethodGenerator(std_string, 'get_path_display_desc',
 methods_node_interface.append(f)
 
 f = CxxMethodGenerator(std_string, 'get_sig',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(xml_string, 'xmlrepr',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(xml_string, 'to_string',
-)
-methods_node_interface.append(f)
-
-f = CxxMethodGenerator(xml_string, 'to_trimmed_string',
 )
 methods_node_interface.append(f)
 
@@ -155,23 +87,18 @@ f = CxxMethodGenerator(void, 'set_context',
 methods_node_interface.append(f)
 
 f = CxxMethodGenerator(void, 'set_prefix_mapping',
-    (std_string_ref, 'prefix', InMode),
-    (std_string_ref, 'uri', InMode),
-)
-methods_node_context.append(f)
-
-f = CxxMethodGenerator(const_char_ptr, 'get_namespace_prefix',
-    (xml_string_ref, 'uri', InMode),
+    (std_string, 'prefix', InMode+RefMode),
+    (std_string, 'uri', InMode+RefMode),
 )
 methods_node_context.append(f)
 
 f = CxxMethodGenerator(net_url, 'resolve_url',
-    (net_url_ref, 'rurl', InMode),
+    (net_url, 'rurl', InMode+RefMode),
 )
 methods_node_context.append(f)
 
 f = CxxMethodGenerator(const_node_ptr, 'get_node',
-    (std_string_ref, 'idd', InMode),
+    (std_string, 'idd', InMode+RefMode),
 )
 methods_node_context.append(f)
 
