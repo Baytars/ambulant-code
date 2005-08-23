9ad4d22a48127ac2b8d9bd74c9dea5c5150d05be
Kees Blom <Kees.Blom@cwi.nl>
2005-08-23 13:01:10 +0000

    First step of IE html-widget. The class CHtmlView is now integrated with AmbulantPlayer, such that the page www.ambulantplayer.org is displayed by IE in AmbulantPlayer's own window. Lots of things are broken now, but it's a fisrt step, on a branch

---
+++ include/ambulant/gui/dx/dx_html_renderer.h
@@ -0,0 +1,105 @@
+/*
+ * 
+ * This file is part of Ambulant Player, www.ambulantplayer.org.
+ * 
+ * Copyright (C) 2003 Stiching CWI, 
+ * Kruislaan 413, 1098 SJ Amsterdam, The Netherlands.
+ * 
+ * Ambulant Player is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Ambulant Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Ambulant Player; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * In addition, as a special exception, if you link Ambulant Player with
+ * other files to produce an executable, this library does not by itself
+ * cause the resulting executable to be covered by the GNU General Public
+ * License. This exception does not however invalidate any other reason why
+ * the executable file might be covered by the GNU General Public License.
+ * 
+ * As a special exception, the copyright holders of Ambulant Player give
+ * you permission to link Ambulant Player with independent modules that
+ * communicate with Ambulant Player solely through the region and renderer
+ * interfaces, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting combined work under
+ * terms of your choice, provided that every copy of the combined work is
+ * accompanied by a complete copy of the source code of Ambulant Player
+ * (the version of Ambulant Player used to produce the combined work),
+ * being distributed under the terms of the GNU General Public License plus
+ * this exception.  An independent module is a module which is not derived
+ * from or based on Ambulant Player.
+ * 
+ * Note that people who make modified versions of Ambulant Player are not
+ * obligated to grant this special exception for their modified versions;
+ * it is their choice whether to do so.  The GNU General Public License
+ * gives permission to release a modified version without this exception;
+ * this exception also makes it possible to release a modified version
+ * which carries forward this exception. 
+ * 
+ */
+
+/* 
+ * @$Id$ 
+ */
+
+#ifndef AMBULANT_GUI_DX_HTML_RENDERER_H
+#define AMBULANT_GUI_DX_HTML_RENDERER_H
+
+#define	WITH_DX_HTML_RENDERER // comment out to disable html renderer
+#ifdef	WITH_DX_HTML_RENDERER
+
+#include "ambulant/config/config.h"
+#include "ambulant/lib/gtypes.h"
+#include "ambulant/net/url.h"
+
+#include <string>
+
+#include "ambulant/common/renderer.h"
+#include "ambulant/gui/dx/dx_playable.h"
+
+namespace ambulant {
+
+namespace gui {
+
+namespace dx {
+
+class viewport;
+
+class dx_html_renderer : public dx_renderer_playable {
+  public:
+	dx_html_renderer(
+		common::playable_notification *context,
+		common::playable_notification::cookie_type cookie,
+		const lib::node *node,
+		lib::event_processor* evp,
+		common::gui_window *window, 
+		dx_playables_context *dxplayer);
+	~dx_html_renderer();
+	void start(double t);
+	void stop();
+	void seek(double t) {}
+	void user_event(const lib::point& pt, int what);
+	void redraw(const lib::screen_rect<int> &dirty, common::gui_window *window);
+	void set_surface(common::surface *dest);
+  private:
+	/*html_widget*/void *m_html_widget;
+};
+
+} // namespace dx
+
+} // namespace gui
+
+} // namespace ambulant 
+
+#endif // WITH_DX_HTML_RENDERER
+
+#endif // AMBULANT_GUI_DX_HTML_RENDERER_H
+
---
+++ include/ambulant/gui/dx/html_bridge.h
@@ -0,0 +1,4 @@
+#include <string>
+
+int create_html_widget(std::string url, int left, int top, int width, int height);
+int delete_html_widget(int id);
