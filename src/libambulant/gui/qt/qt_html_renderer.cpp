415bd86c5d6c2dfd8ff2bf1f4e38230f1d7b596c
Jack Jansen <Jack.Jansen@cwi.nl>
2005-10-04 14:32:12 +0000

    Got code to compile again.

--- src/libambulant/gui/qt/qt_html_renderer.cpp
+++ src/libambulant/gui/qt/qt_html_renderer.cpp
@@ -65,6 +65,21 @@
 using namespace ambulant;
 using namespace gui::qt;
 
+// Unique key used to access our renderer_private data
+static common::renderer_private_id my_renderer_id = (common::renderer_private_id)"qt_html_renderer";
+
+class gui::qt::browser_container : public ref_counted_obj {
+  public:
+    KHTMLPart *m_browser;
+    
+  browser_container(KHTMLPart *br)
+  : m_browser(br) {}
+  
+  ~browser_container() {
+    m_browser->hide();
+  }
+};
+
 qt_html_renderer::qt_html_renderer(
 		common::playable_notification *context,
 		common::playable_notification::cookie_type cookie,
@@ -80,22 +95,62 @@ qt_html_renderer::qt_html_renderer(
 
 void 
 gui::qt::qt_html_renderer::start(double t) {
+	m_lock.enter();
  	AM_DBG lib::logger::get_logger()->debug("qt_html_renderer::start(0x%x)", this);
+
+	assert(!m_html_browser);
+	m_html_browser = dynamic_cast<browser_container*>(m_dest->get_renderer_private_data(my_renderer_id));
+	if (m_html_browser == NULL) {
+		lib::rect rc = m_dest->get_rect();
+		const lib::point p = m_dest->get_global_topleft();
+		rc.translate(p);
+
+        assert(m_dest);
+        common::gui_window* window = m_dest->get_gui_window();
+        assert(window);
+        ambulant_qt_window* aqw = (ambulant_qt_window*)window;
+        qt_ambulant_widget* qaw = aqw->get_ambulant_widget();
+        assert(qaw);
+
+		KHTMLPart *br = new KHTMLPart((QWidget*)qaw);
+		// XXX new html_browser(rc.left(), rc.top(), rc.width(), rc.height());
+		assert(br);
+		KHTMLView *vw = br->view();
+		assert(vw);
+		vw->move(rc.left(), rc.top());
+		vw->resize(rc.width(), rc.height());
+		m_html_browser = new browser_container(br);
+		m_dest->set_renderer_private_data(my_renderer_id, static_cast<common::renderer_private_data*>(m_html_browser));
+	}
+	assert(m_html_browser);
+	/*AM_DBG*/ lib::logger::get_logger()->debug("qt_html_renderer::start(0x%x) html_widget=0x%x",this,m_html_browser->m_browser);
+
+	net::url url = m_node->get_url("src");
+	m_html_browser->m_browser->openURL(url.get_url().c_str());
+
+	m_html_browser->m_browser->show();
+
+	m_dest->show(this);
+	m_dest->need_events(m_wantclicks);
+	m_activated = true;
+
+	m_lock.leave();
 }
 
 qt_html_renderer::~qt_html_renderer() {
 	m_lock.enter();
 	AM_DBG lib::logger::get_logger()->debug("~qt_html_renderer(0x%x)", this);
-	if (m_html_browser) {
+//	if (m_html_browser) {
 // the following hide() causes flicker, is needed to clear at the end
-		m_html_browser->hide();
+//		m_html_browser->hide();
 //	m_html_widget is not deleted, but remembered in the surface_impl
 //		delete m_html_browser;
-		m_html_browser = NULL;
-	}
+//		m_html_browser = NULL;
+//	}
 	m_lock.leave();
 }
 
+#ifdef JACK_THINKS_THIS_IS_WRONG
 void
 gui::qt::qt_html_renderer::set_surface(common::surface *dest) {
 	m_lock.enter();
@@ -103,9 +158,10 @@ gui::qt::qt_html_renderer::set_surface(common::surface *dest) {
 
 	m_dest = dest;
 	
+	m_html_browser = dynamic_cast<browser_container*>(m_dest->get_renderer_private_data(my_renderer_id));
 	lib::rect rc = dest->get_rect();
 	const lib::point p = dest->get_global_topleft();
-	m_url = m_node->get_url("src");
+	net::url url = m_node->get_url("src");
 
 // following test works on WIN32, fails on linux when url contains #tag
 //	if(!lib::memfile::exists(m_url)) {
@@ -134,6 +190,7 @@ gui::qt::qt_html_renderer::set_surface(common::surface *dest) {
 	m_context->stopped(m_cookie);
 	m_lock.leave();
 }
+#endif
 
 void
 gui::qt::qt_html_renderer::stop() {
@@ -144,9 +201,11 @@ gui::qt::qt_html_renderer::stop() {
 //		m_html_browser->hide();
 	m_dest->renderer_done(this);
 	m_activated = false;
+	// XXX Call stopped()?
 	m_lock.leave();
 }
 
+#ifdef JACK_THINKS_THIS_IS_WRONG
 void
 gui::qt::qt_html_renderer::user_event(const lib::point& pt, int what) {
 	m_lock.enter();
@@ -193,4 +252,5 @@ gui::qt::qt_html_renderer::redraw(const lib::rect& r, common::gui_window *window
 	m_html_browser->show();
 	m_lock.leave();
 }
+#endif
 #endif/*WITH_QT_HTML_WIDGET*/
