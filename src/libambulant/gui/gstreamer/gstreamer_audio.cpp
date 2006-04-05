ae3bf6e97d4683f19f3d75451bd5a2c1c7143c1c
Kees Blom <Kees.Blom@cwi.nl>
2006-04-05 10:00:29 +0000

    Added a pthread_mutex (instead of abusing a critical region) to the gstreamer player prperly synchronize.

--- src/libambulant/gui/gstreamer/gstreamer_audio.cpp
+++ src/libambulant/gui/gstreamer/gstreamer_audio.cpp
@@ -17,7 +17,7 @@
 // along with Ambulant Player; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#define AM_DBG
+//#define AM_DBG
 #ifndef AM_DBG
 #define AM_DBG if(0)
 #endif
@@ -32,10 +32,11 @@ using namespace ambulant;
 using namespace gui::gstreamer;
 
 static void
-lock_release(void* rend);
-
+mutex_acquire(void* player);
 static void
-pipeline_store(void* rend, GstElement* p);
+mutex_release(void* player);
+static void
+pipeline_store(void* player, GstElement* p);
 
 extern "C" {
 
@@ -49,12 +50,11 @@ mp3player_error_cb (GstElement * bin, GstElement * error_element,
 		    gpointer user_data);
 
 int
-gst_mp3_player(const char* uri, GstElement** gst_player_p, gstreamer_audio_renderer* rend, gboolean** player_done_p)
+gst_mp3_player(const char* uri, GstElement** gst_player_p, gstreamer_player* gstreamer_player, gboolean* player_done_p)
 {
   GMainLoop *loop;
   GstElement *source=NULL,*sink=NULL, *pipeline=NULL;
   char **files = NULL;
-  gboolean gst_mp3_player_done = FALSE;
   const char* id = "gst_mp3_player";
   AM_DBG g_print ("%s: %s\n", id, "starting");
 
@@ -63,12 +63,11 @@ gst_mp3_player(const char* uri, GstElement** gst_player_p, gstreamer_audio_rende
 #ifdef  WITH_NOKIA770
   source   = gst_element_factory_make ("gnomevfssrc", "source"); 
   sink     = gst_element_factory_make ("dspmp3sink", "sink"); 
-  if (gst_player_p) *gst_player_p = pipeline;
-  if (player_done_p) *player_done_p = &gst_mp3_player_done;
 #else //WITH_NOKIA770
   source   = gst_element_factory_make ("playbin", "playbin"); 
-  if (gst_player_p) *gst_player_p = source;
+  if (gst_player_p) *gst_player_p = pipeline;
 #endif//WITH_NOKIA770
+  if (gst_player_p) *gst_player_p = pipeline;
 
   if ( !( pipeline && source
 #ifdef  WITH_NOKIA770
@@ -77,20 +76,25 @@ gst_mp3_player(const char* uri, GstElement** gst_player_p, gstreamer_audio_rende
 	  ) ) {
     g_print ("%s:", "gst_mp3_player");
     if ( ! pipeline) g_print (" %s() failed", "get_pipeline_new");
+#ifdef  WITH_NOKIA770
     if ( ! source)   g_print (" %s=%s(%s) failed", "source", 
 			      "gst_element_factory_make", "gnomevfssrc");
-#ifdef  WITH_NOKIA770
     if ( ! sink)   g_print (" %s=%s(%s) failed", "sink", 
 			      "gst_element_factory_make", "dspmp3sink");
-
+#else //WITH_NOKIA770
+    if ( ! source)   g_print (" %s=%s(%s) failed", "source", 
+			      "gst_element_factory_make", "playbin");
 #endif//WITH_NOKIA770
     g_print ("\n");
-    lock_release(rend);
     return -1;
   }
-  g_print ("%s: %s\n", id, "set the source audio file");
+  AM_DBG g_print ("%s: %s\n", id, "set the source audio file");
   /* set the source audio file */
+#ifdef  WITH_NOKIA770
   g_object_set (G_OBJECT(source), "location", uri, NULL);
+#else //WITH_NOKIA770
+  g_object_set (G_OBJECT(source), "uri", uri, NULL);
+#endif//WITH_NOKIA770
 
   /* put all elements  to the main pipeline */
   gst_bin_add_many (GST_BIN(pipeline), source,
@@ -99,34 +103,41 @@ gst_mp3_player(const char* uri, GstElement** gst_player_p, gstreamer_audio_rende
 #endif//WITH_NOKIA770
 		    NULL);
 
+#ifdef  WITH_NOKIA770
   /* link the elements */
   gst_element_link (source, sink);
+#endif//WITH_NOKIA770
 
+
+  /* wait for start */
+  gst_element_set_state (pipeline, GST_STATE_PAUSED);
+//gst_element_set_state (GST_ELEMENT(pipeline), GST_STATE_PAUSED);
 
   AM_DBG g_print ("%s: %s\n", id, "add call-back message handler for eos");
   /* add call-back message handlers to check for eos and errors */
   g_signal_connect (GST_BIN(pipeline), "eos",
-		    G_CALLBACK (mp3player_eos_cb), &gst_mp3_player_done);
+		    G_CALLBACK (mp3player_eos_cb), player_done_p);
   AM_DBG g_print ("%s: %s\n", id, "add call-back message handler for error");
   g_signal_connect (GST_BIN(pipeline), "error",
-		    G_CALLBACK (mp3player_error_cb), &gst_mp3_player_done);
+		    G_CALLBACK (mp3player_error_cb), player_done_p);
 
   AM_DBG g_print ("%s: %s\n", id, "wait for start");
-  /* wait for start */
-  gst_element_set_state (GST_ELEMENT(pipeline), GST_STATE_PAUSED);
 
-  lock_release(rend);
-  pipeline_store(rend, pipeline);
+  pipeline_store(gstreamer_player, pipeline);
+  mutex_release(gstreamer_player);
 
   AM_DBG g_print ("%s: %s\n", id, "iterate");
    /* iterate */
-  if ( ! gst_mp3_player_done) g_print ("Now playing %s ...", uri);
-  while ( ! gst_mp3_player_done) {
-     gst_bin_iterate (GST_BIN(pipeline));
+  if ( ! *player_done_p) g_print ("Now playing %s ...", uri);
+  while ( ! *player_done_p) {
+    gst_bin_iterate (GST_BIN(pipeline));
   }
+  AM_DBG if (player_done_p) g_print (" done !\n"); 
+
+  mutex_acquire(gstreamer_player); // to be released by the caller
+
   /* stop the pipeline */
   gst_element_set_state (GST_ELEMENT(pipeline), GST_STATE_NULL);
-  AM_DBG if (gst_mp3_player_done) g_print (" done !\n"); 
 
   /* cleanup */
   gst_object_unref (GST_OBJECT(pipeline));
@@ -138,10 +149,10 @@ static void
 mp3player_eos_cb (GstElement *pipeline,gpointer user_data)
 {
   const char* id = "mp3player_eos_cb";
-  gboolean *p_gst_mp3_player_done = (gboolean *) user_data;
+  gboolean *p_player_done = (gboolean *) user_data;
 
   AM_DBG g_print ("%s: %s\n", id, "called");
-  *p_gst_mp3_player_done = TRUE;
+  *p_player_done = TRUE;
 }
 
 static void
@@ -150,13 +161,13 @@ mp3player_error_cb (GstElement *pipeline, GstElement *error_element,
 gpointer user_data)
 {
   const char* id = "mp3player_error_cb";
-  gboolean *p_gst_mp3_player_done = (gboolean *) user_data;
+  gboolean *p_player_done = (gboolean *) user_data;
 
   AM_DBG g_print ("%s: %s\n", id, "called");
   if (error)
     g_printerr ("Error: %s", error->message);
 
-  *p_gst_mp3_player_done = TRUE;
+  *p_player_done = TRUE;
 }
 }
 
@@ -218,10 +229,10 @@ gstreamer_audio_renderer::~gstreamer_audio_renderer()
 	m_lock.enter();
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer::~gstreamer_audio_renderer(0x%x) m_url=%s",  this, m_url.get_url().c_str());		
 	if (m_is_playing) {
-		m_lock.leave();
+	//	m_lock.leave();
 		//TBD stop it
 		m_player->stop();
-		m_lock.enter();
+	//	m_lock.enter();
 	}	
 #ifdef USE_SMIL21
 	if (m_transition_engine) {
@@ -235,22 +246,10 @@ gstreamer_audio_renderer::~gstreamer_audio_renderer()
 	m_lock.leave();
 }
 
-void
-gstreamer_audio_renderer::lock_release() {
-	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer::lock_release(0x%x)",  this);
-	m_lock.leave();
-}
-
 static void
 pipeline_store(void* obj, GstElement*pipeline) {
 	gstreamer_audio_renderer* rend = (gstreamer_audio_renderer*) obj;
 	if (rend) rend->m_pipeline = pipeline;
-}
-
-static void
-lock_release(void* obj) {
-	gstreamer_audio_renderer* rend = (gstreamer_audio_renderer*) obj;
-	if (rend) rend->lock_release();
 }
 
 void
@@ -260,8 +259,9 @@ gstreamer_audio_renderer::init_player(const lib::node *node) {
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer::init_player(0x%x) url=",  this, m_url.get_url().c_str());
 	_init_clip_begin_end();
 	m_player = new gstreamer_player(m_url.get_url().c_str(), this);
-	m_lock.enter(); // thread will release the lock
-	m_player->start();
+	m_player->init();
+	m_context->started(m_cookie, 0);
+	//KB	if (gst_player failed)m_context->stopped(m_cookie, 0);
 }
 #ifdef USE_SMIL21
 
@@ -322,10 +322,10 @@ gstreamer_audio_renderer::stop()
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer::stop(0x%x)",(void*)this);
 	if (m_is_playing) {
 		m_lock.leave();
-		// XXX Should we call stopped_callback?
-		// m_context->stopped(m_cookie, 0);
 		m_player->stop();
 		m_lock.enter();
+		// XXX Should we call stopped_callback?
+		m_context->stopped(m_cookie, 0);
 	}
 	m_is_playing = false;
 	m_lock.leave();
@@ -336,7 +336,8 @@ gstreamer_audio_renderer::pause()
 {
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer.pause(0x%x)", (void *)this);
 	m_lock.enter();
-	gst_element_set_state (m_player->gst_player(), GST_STATE_PAUSED);
+	if (m_player)
+		m_player->pause();
 	m_is_paused = true;
 	m_lock.leave();
 }
@@ -346,7 +347,8 @@ gstreamer_audio_renderer::resume()
 {
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer.resume(0x%x)", (void *)this);
 	m_lock.enter();
-	gst_element_set_state (m_player->gst_player(), GST_STATE_PLAYING);
+	if (m_player)
+		m_player->play();
 	m_is_playing = true;
 	m_is_paused = false;
 	m_lock.leave();
@@ -366,16 +368,20 @@ void
 gstreamer_audio_renderer::seek(double where)
 {
        lib::logger::get_logger()->trace("gstreamer_audio_renderer: seek(%f)", where);
+       m_lock.enter();
        if (m_player) {
               double microsec = 1e6;
 	      guint64 where_guint64;
 	      where += (m_clip_begin / microsec);
 	      where_guint64 = llrint(where)* GST_SECOND;	      
 	      lib::logger::get_logger()->trace("gstreamer_audio_renderer: seek() where=%f, where_guint64=%lu", where, where_guint64);
+	      m_player->mutex_acquire();
 	      if ( ! gst_element_seek(m_player->gst_player(), (GstSeekType) (GST_SEEK_METHOD_SET | GST_FORMAT_TIME | GST_SEEK_FLAG_FLUSH), where_guint64)) {
 	             lib::logger::get_logger()->trace("gstreamer_audio_renderer: seek() failed.");
 	      }
+	      m_player->mutex_release();
        }
+       m_lock.leave();
 }
 
 common::duration 
@@ -387,23 +393,25 @@ gstreamer_audio_renderer::get_dur()
 
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_audio_renderer.get_dur(0x%x)", (void *)this);
 	m_lock.enter();
-	if (m_dest) {
-	       gst_element_query(m_player->gst_player(), GST_QUERY_TOTAL, &fmtTime, &length);
+	if (m_player) {
+	        m_player->mutex_acquire();
+		gst_element_query(m_player->gst_player(), GST_QUERY_TOTAL, &fmtTime, &length);
+		m_player->mutex_release();
 	}
 	m_lock.leave();
 
 	if (length != -1) {
-	       double nanosec = 1e9, microsec = 1e6;
-	       double dur = double(length) / nanosec;
-	       double clip_begin = m_clip_begin / microsec;
-	       double clip_end = m_clip_end / microsec;
+	        double nanosec = 1e9, microsec = 1e6;
+		double dur = double(length) / nanosec;
+		double clip_begin = m_clip_begin / microsec;
+		double clip_end = m_clip_end / microsec;
 
-	       if (clip_end > 0 && dur > clip_end)
-	              dur = clip_end;
-	       if (clip_begin > 0)
-	              dur -= clip_begin;
-	       lib::logger::get_logger()->trace("gstreamer_audio_renderer: get_dur() clip_begin=%f clip_end=%f dur=%f", clip_begin, clip_end, dur);
-	       rv = common::duration(true, dur);
+		if (clip_end > 0 && dur > clip_end)
+	               dur = clip_end;
+		if (clip_begin > 0)
+	               dur -= clip_begin;
+		lib::logger::get_logger()->trace("gstreamer_audio_renderer: get_dur() clip_begin=%f clip_end=%f dur=%f", clip_begin, clip_end, dur);
+		rv = common::duration(true, dur);
   	}
 	return rv;
 }
@@ -421,32 +429,108 @@ gstreamer_player::gstreamer_player(const char* uri, gstreamer_audio_renderer* re
 
 gstreamer_player::~gstreamer_player() {
 	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::~gstreamer_player()(0x%x) m_uri=%s", (void*)this, m_uri);
-	stop();
-	delete m_uri;
-}
-
-unsigned long
-gstreamer_player::run() {
-	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::run(0x%x)m_uri=%s", (void*)this, m_uri);
-        gst_mp3_player (m_uri, &m_gst_player, m_audio_renderer, &m_player_done);
-}
-
-unsigned long
-gstreamer_player::init() {
-	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::start(0x%x)m_uri=%s", (void*)this, m_uri);
-        start();
-	return 0;
-}
-
-void
-gstreamer_player::stop() {
-	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::stop(0x%x)m_uri=%s", (void*)this, m_uri);
-	mp3player_eos_cb(m_gst_player, m_player_done);
+	stop_player();
+	if (m_uri) free(m_uri);
+	m_uri = NULL;
+	// static mutex needs to exists during the lifetime of the program
+	//  if (pthread_mutex_destroy(&s_mutex) < 0) {
+	//  	    lib::logger::get_logger()->fatal("gstreamer_player: pthread_mutex_destroy failed: %s", strerror(errno));
+        // }
 }
 
 GstElement*
 gstreamer_player::gst_player() {
 	return m_gst_player;
+}
+
+unsigned long
+gstreamer_player::run() {
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::run(0x%x)m_uri=%s", (void*)this, m_uri);
+	gst_mp3_player (m_uri, &m_gst_player, this, &m_player_done);
+	m_gst_player = NULL;
+	mutex_release(); // lock was acquired by gst_mp3_player()
+	// inform the scheduler that the gstreamer player has terminated
+	m_audio_renderer->stop();
+}
+
+unsigned long
+gstreamer_player::init() {
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::init(0x%x)m_uri=%s", (void*)this, m_uri);
+	mutex_acquire(); // lock will be released by gst_mp3_player()
+	m_player_done = FALSE;
+	start();	     // starts run() in separate thread
+	mutex_acquire(); // wait until gst_mp3_player() is initialized
+	mutex_release();
+	return 0;
+}
+
+void
+gstreamer_player::stop_player() {
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::stop_player(0x%x)m_uri=%s", (void*)this, m_uri);
+	mutex_acquire(); 
+	if (m_gst_player)
+		mp3player_eos_cb(m_gst_player, &m_player_done);
+	mutex_release(); 
+}
+
+void
+gstreamer_player::pause() {
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::pause(0x%x)m_uri=%s", (void*)this, m_uri);
+	mutex_acquire(); 
+	if (m_gst_player)
+		gst_element_set_state (m_gst_player, GST_STATE_PAUSED);
+	mutex_release(); 
+}
+
+void
+gstreamer_player::play() {
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::play(0x%x)m_uri=%s", (void*)this, m_uri);
+	mutex_acquire(); 
+	if (m_gst_player)
+		gst_element_set_state (m_gst_player, GST_STATE_PLAYING);
+	mutex_release(); 
+}
+
+void
+gstreamer_player::mutex_acquire() {
+	if ( ! s_initialized) {
+		if (pthread_mutex_init(&s_mutex, NULL) < 0) {
+                	lib::logger::get_logger()->fatal("gstreamer_player:::mutex_acquire() pthread_mutex_init failed: %s", strerror(errno));
+		}
+		s_initialized = true;
+	}
+	if (pthread_mutex_lock(&s_mutex) < 0) {
+                lib::logger::get_logger()->fatal("gstreamer_player::mutex_acquire(): pthread_mutex_lock failed: %s", strerror(errno));
+	}
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::mutex_acquire(0x%x): pthread_mutex_lock called.", this);
+}
+
+void
+gstreamer_player::mutex_release() {
+	if ( ! s_initialized) {
+                lib::logger::get_logger()->fatal("gstreamer_player::mutex_release() called while not initialized");
+	}
+	AM_DBG lib::logger::get_logger()->debug("gstreamer_player::mutex_release(0x%x): pthread_mutex_unlock called.", this);
+	if (pthread_mutex_unlock(&s_mutex) < 0) {
+             	lib::logger::get_logger()->fatal("gstreamer_player::mutex_release() pthread_mutex_unlock failed: %s", strerror(errno));
+	}
+}
+
+
+static void
+mutex_acquire(void* obj) {
+        gstreamer_player* player = (gstreamer_player*) obj;
+        if (player) {
+	  player->mutex_acquire();
+        }
+}
+
+static void
+mutex_release(void* obj) {
+        gstreamer_player* player = (gstreamer_player*) obj;
+        if (player) {
+	        player->mutex_release();
+        }
 }
 
 /* gstreamer_renderer_factory *********************************************/
