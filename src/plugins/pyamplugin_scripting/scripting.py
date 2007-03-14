eab1ee214cbc7794f53db6636cf4e64c8bf2b65f
Jack Jansen <Jack.Jansen@cwi.nl>
2007-03-14 16:25:08 +0000

    expr attribute starts to work too.

--- src/plugins/pyamplugin_scripting/scripting.py
+++ src/plugins/pyamplugin_scripting/scripting.py
@@ -13,23 +13,36 @@ class MyScriptComponentFactory(ambulant.script_component_factory):
 class MyScriptComponent(ambulant.script_component):
     def __init__(self):
         print 'MyScriptComponent()'
+        self.scope = {}
+        # What dowe want to export to scope???
         
     def declare_state(self, state):
         print 'declare_state, node=', state
-        import pdb
-        pdb.set_trace()
+        statements = state.get_trimmed_data()
+        exec statements in self.scope, self.scope
+        #import pdb
+        #pdb.set_trace()
         
     def bool_expression(self, expr):
         print 'bool_expression, expr=', expr
-        return False
+        rv = eval(expr, self.scope, self.scope)
+        print 'bool_expression returning', rv
+        rv = not not rv
+        print 'bool_expression returning casted', rv
+        return rv
         
     def set_value(self, var, expr):
         print 'set_value, var=', var, 'expr=', expr
+        exec "%s = %s" % (var, expr) in self.scope, self.scope
         
     def send(self, submission):
         print 'send, submission=', submission
         
     def string_expression(self, expr):
         print 'string_expression, expr=', expr
-        return ''
+        rv = eval(expr, self.scope, self.scope)
+        print 'string_expression returning', rv
+        rv = str(rv)
+        print 'string_expression returning casted', rv
+        return rv
         
