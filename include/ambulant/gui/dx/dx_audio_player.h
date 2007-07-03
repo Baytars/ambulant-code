97888aacd33c7fcd0b4a6822f75b5cf112467c55
Jack Jansen <Jack.Jansen@cwi.nl>
2007-07-03 13:55:28 +0000

    Implemented getting the control interface, and registering the players so we can actually control them.

--- include/ambulant/gui/dx/dx_audio_player.h
+++ include/ambulant/gui/dx/dx_audio_player.h
@@ -54,8 +54,53 @@
 // filter into our filtergraph. This allows for changing audio playback
 // speed without altering pitch.
 // If the TPB DirectShow filter is not available we do nothing.
+#include <set>
+
 DEFINE_GUID(CLSID_TPBVupp10, 
 	0x66172967, 0x56c5, 0x4b89, 0xaa, 0x92, 0xc9, 0xef, 0xec, 0x56, 0x46, 0x7b);
+
+// {A33E626E-D6C4-4559-A1D6-9F1D95F0D8E2}
+DEFINE_GUID(IID_IVuppInterface, 
+0xa33e626e, 0xd6c4, 0x4559, 0xa1, 0xd6, 0x9f, 0x1d, 0x95, 0xf0, 0xd8, 0xe2);
+
+DECLARE_INTERFACE_(IVuppInterface, IUnknown) { 
+
+	//Deklarera metoder:
+	STDMETHOD(setCrossFadeSpeed)
+		( THIS_
+			double speed 
+		) PURE;
+
+	STDMETHOD(setWindowLength)
+		( THIS_
+			int length
+		) PURE;
+
+	STDMETHOD(setCycleSpeed)
+		( THIS_
+			short speed
+		) PURE;
+
+	STDMETHOD(setSilenceLoudnessThreshold)
+		( THIS_
+			short threshold
+		) PURE;
+
+	STDMETHOD(setSilenceRemainderLength)
+		( THIS_
+			short length
+		) PURE;
+
+	STDMETHOD(setSilenceSpeed)
+		( THIS_
+			short speed
+		) PURE;
+
+	STDMETHOD(getPosition)
+		( THIS_
+			LONGLONG &position
+		) PURE;
+};
 #endif // WITH_TPB_AUDIO_SPEEDUP
 
 namespace ambulant {
@@ -113,7 +158,13 @@ class audio_player : public common::playable {
 	IMediaEvent *m_media_event;
 	IBasicAudio *m_basic_audio;
 #ifdef WITH_TPB_AUDIO_SPEEDUP
+	IVuppInterface *m_audio_speedup;
 	void initialize_speedup_filter();
+	void set_playback_rate(double rate);
+	static void register_player(audio_player *cur);
+	static void unregister_player(audio_player *cur);
+	static void set_global_playback_rate(double rate);
+	static std::set<audio_player *> s_active_players;
 #endif
 };
 	
