4a012711983dff982c747c668ce4d8ab82eb5300
Jack Jansen <Jack.Jansen@cwi.nl>
2007-08-31 11:38:21 +0000

    dsvideo is starting to work. Scaling and such don't seem to work yet, and there's too much copying going on, but at least we see something.

--- include/ambulant/gui/dx/dx_dsvideo.h
+++ include/ambulant/gui/dx/dx_dsvideo.h
@@ -35,6 +35,8 @@
 #endif
 #include "ambulant/common/video_renderer.h"
 #include "ambulant/lib/mtsync.h"
+#include "ambulant/gui/dx/dx_transition.h"
+#include "ambulant/gui/dx/dx_surface.h"
 
 namespace ambulant {
 
@@ -45,11 +47,12 @@ namespace gui {
 
 namespace dx {
 
-class video_dib_surface;
 
 class dx_dsvideo_renderer : 
 	public common::video_renderer {
   public:
+	typedef lib::color_trible ColorType;
+
 	dx_dsvideo_renderer(
 		playable_notification *context,
 		playable_notification::cookie_type cookie,
@@ -64,10 +67,15 @@ class dx_dsvideo_renderer :
 	void redraw(const rect &dirty, gui_window *window);
 	void set_intransition(const lib::transition_info *info) {};
 	void start_outtransition(const lib::transition_info *info) {};
+	// XXXJACK: need to copy code from dx_renderer_playable
+	gui::dx::dx_transition *get_transition() { return NULL; }
   private:
-    void _init_surfaces(gui_window *window);
-    video_dib_surface *m_dibsurf;	// Temporary storage for image data
-	IDirectDrawSurface *m_ddsurf;	// Where we are drawing to
+    void _init_bitmap();
+	void _init_ddsurf(gui_window *window);
+	void _copy_to_ddsurf();
+	HBITMAP m_bitmap;			// Bitmap to hold the image
+	char *m_bitmap_dataptr;		// Pointer to actual data inside bitmap
+	IDirectDrawSurface *m_ddsurf;	// DD surface, for scaling and such
 	critical_section m_lock;
 };
 
--- src/libambulant/gui/dx/dx_dsvideo.cpp
+++ src/libambulant/gui/dx/dx_dsvideo.cpp
@@ -26,6 +26,7 @@
 #include "ambulant/common/smil_alignment.h"
 #include "ambulant/gui/dx/dx_window.h"
 #include "ambulant/gui/dx/dx_viewport.h"
+#include "ambulant/gui/dx/img_decoder.h"
 #include "ambulant/lib/win32/win32_error.h"
 using ambulant::lib::win32::win_report_error;
 
@@ -54,19 +55,42 @@ dx_dsvideo_renderer::dx_dsvideo_renderer(
 	event_processor *evp,
 	common::factories *factory)
 :	common::video_renderer(context, cookie, node, evp, factory),
-	m_ddsurf(NULL),
-	m_dibsurf(NULL)
+	m_bitmap(NULL),
+	m_bitmap_dataptr(NULL),
+	m_ddsurf(NULL)
 {
 	AM_DBG lib::logger::get_logger()->debug("dx_dsvideo_renderer(): 0x%x created", (void*)this);
 }
 
 void
-dx_dsvideo_renderer::_init_surfaces(common::gui_window *window)
+dx_dsvideo_renderer::_init_bitmap()
 {
+	// First check that we actually have the necessary data (x/y size)
+	if (m_size.h == 0 || m_size.w == 0) return;
+	assert(m_bitmap == NULL);
+	assert(m_bitmap_dataptr == NULL);
+	// Create a HBITMAP for which we also have the data pointer (so we can
+	// copy frames into it later)
+	void *pBits = NULL;
+	BITMAPINFO *pbmpi = get_bmp_info(m_size.w, -m_size.h, 32);
+	m_bitmap = CreateDIBSection(NULL, pbmpi, DIB_RGB_COLORS, &pBits, NULL, 0);
+	if(m_bitmap==NULL || pBits==NULL) {
+		lib::logger::get_logger()->error("CreateDIBSection() failed");
+		return; // failed
+	}
+	m_bitmap_dataptr = (char *)pBits;
+}
+
+void
+dx_dsvideo_renderer::_init_ddsurf(gui_window *window)
+{
+	if (m_size.h == 0 || m_size.w == 0) return;
+	assert(m_ddsurf == NULL);
 	dx_window *dxwindow = static_cast<dx_window*>(window);
 	viewport *v = dxwindow->get_viewport();
+	assert(v);
 	m_ddsurf = v->create_surface(m_size);
-	// m_dibsurf = xxxx;
+;
 }
 
 dx_dsvideo_renderer::~dx_dsvideo_renderer()
@@ -76,52 +100,139 @@ dx_dsvideo_renderer::~dx_dsvideo_renderer()
 	if (m_ddsurf)
 		m_ddsurf->Release();
 	m_ddsurf = NULL;
-	if (m_dibsurf)
-			delete m_dibsurf;
-	m_dibsurf = 0;
+	if (m_bitmap)
+			DeleteObject(m_bitmap);
+	m_bitmap = NULL;
+	m_bitmap_dataptr = NULL;
 	m_lock.leave();
 }
 	
 void
 dx_dsvideo_renderer::show_frame(const char* frame, int size)
 {
-	if (m_ddsurf == NULL || m_dibsurf == NULL) return;
+	if (m_bitmap == NULL) _init_bitmap();
 	m_lock.enter();
 	AM_DBG lib::logger::get_logger()->debug("dx_dsvideo_renderer::show_frame: size=%d", size);
 	assert(size == (int)(m_size.w * m_size.h * 4));
-	// xxx_copy(frame, m_dibsurf, size);
+	memcpy(m_bitmap_dataptr, frame, size);
+	if (m_dest) m_dest->need_redraw();
+	m_lock.leave();
+}
+
+void
+dx_dsvideo_renderer::_copy_to_ddsurf()
+{
+	if (m_bitmap == NULL || m_ddsurf == NULL) return;
 	HRESULT hr;
 	HDC hdc = ::GetDC(NULL);
 	hr = m_ddsurf->GetDC(&hdc);
 	if (FAILED(hr)) {
 		win_report_error("DirectDrawSurface::GetDC()", hr);
-		m_lock.leave();
 		return;
 	}
 	HDC bmp_hdc = CreateCompatibleDC(hdc);
-	HBITMAP hbmp_old = (HBITMAP) SelectObject(bmp_hdc, m_dibsurf->get_handle());
+	HBITMAP hbmp_old = (HBITMAP) SelectObject(bmp_hdc, m_bitmap);
 	::BitBlt(hdc, 0, 0, m_size.w, m_size.h, bmp_hdc, 0, 0, SRCCOPY);
 	SelectObject(bmp_hdc, hbmp_old);
 	DeleteDC(bmp_hdc);
 	m_ddsurf->ReleaseDC(hdc);
-	if (m_dest) m_dest->need_redraw();
-	m_lock.leave();
 }
 
 void
 dx_dsvideo_renderer::redraw(const rect &dirty, gui_window *window)
 {
 	m_lock.enter();
-	if (m_ddsurf == NULL || m_dibsurf == NULL) {
-		// This is really the wrong place to initialize the surfaces: it
-		// will make us lose the first frame. Need to rethink, maybe do in
-		// set_surface?
-		_init_surfaces(window);
+	// XXXX Should do this only when needed (skip extra bitblit)
+	if (m_ddsurf == NULL ) _init_ddsurf(window);
+	if (m_ddsurf == NULL) {
 		m_lock.leave();
 		return;
 	}
-	const rect &r = m_dest->get_rect();
-	AM_DBG logger::get_logger()->debug("dx_dsvideo_renderer.redraw(0x%x, local_ltrb=(%d,%d,%d,%d)", (void *)this, r.left(), r.top(), r.right(), r.bottom());
+	// XXXX Should do this only when needed:
+	_copy_to_ddsurf();
+	//const rect &r = m_dest->get_rect();
+	//AM_DBG logger::get_logger()->debug("dx_dsvideo_renderer.redraw(0x%x, local_ltrb=(%d,%d,%d,%d)", (void *)this, r.left(), r.top(), r.right(), r.bottom());
+#if 1
+	lib::rect img_rect1;
+	lib::rect img_reg_rc;
+	// lib::size srcsize = m_image->get_size();
+
+#ifdef WITH_SMIL30
+	lib::rect croprect = m_dest->get_crop_rect(m_size);
+	img_reg_rc = m_dest->get_fit_rect(croprect, srcsize, &img_rect1, m_alignment);
+#else
+	// Get fit rectangles
+	img_reg_rc = m_dest->get_fit_rect(m_size, &img_rect1, m_alignment);
+#endif
+	// Use one type of rect to do op
+	lib::rect img_rect(img_rect1);
+	
+	// A complete repaint would be:  
+	// {img, img_rect } -> img_reg_rc
+	
+	// We have to paint only the intersection.
+	// Otherwise we will override upper layers 
+	lib::rect img_reg_rc_dirty = img_reg_rc & dirty;
+	if(img_reg_rc_dirty.empty()) {
+		// this renderer has no pixels for the dirty rect
+		AM_DBG lib::logger::get_logger()->debug("dx_dsvideo_renderer::redraw NOT: empty dirty region %0x %s ", m_dest, m_node->get_url("src").get_url().c_str());
+		m_lock.leave();
+		return;
+	}	
+	
+	// Find the part of the image that is mapped to img_reg_rc_dirty
+	lib::rect img_rect_dirty = reverse_transform(&img_reg_rc_dirty, 
+		&img_rect, &img_reg_rc);
+		
+	// Translate img_reg_rc_dirty to viewport coordinates 
+	lib::point topleft = m_dest->get_global_topleft();
+	img_reg_rc_dirty.translate(topleft);
+	
+	
+	// Finally blit img_rect_dirty to img_reg_rc_dirty
+	AM_DBG lib::logger::get_logger()->debug("dx_img_renderer::redraw %0x %s ", m_dest, m_node->get_url("src").get_url().c_str());
+	
+	// Get the top-level surface
+	dx_window *dxwindow = static_cast<dx_window*>(window);
+	viewport *v = dxwindow->get_viewport();
+	if(!v) {
+		AM_DBG lib::logger::get_logger()->debug("dx_img_renderer::redraw NOT: no viewport %0x %s ", m_dest, m_node->get_url("src").get_url().c_str());
+		return;
+	}
+
+	dx_transition *tr = get_transition();
+	if (tr && tr->is_fullscreen()) {
+		v->set_fullscreen_transition(tr);
+		tr = NULL;
+	}
+
+	if(tr && tr->is_outtrans()) {
+		// First draw the background color, if applicable
+		const common::region_info *ri = m_dest->get_info();
+		if(ri)
+			v->clear(img_reg_rc_dirty,ri->get_bgcolor(), ri->get_bgopacity());
+		// Next, take a snapshot of the relevant pixels as they are now, before we draw the image
+		IDirectDrawSurface *bgimage = v->create_surface(m_size);
+		lib::rect dirty_screen = img_rect_dirty;
+		dirty_screen.translate(topleft);
+		RECT bgrect_image, bgrect_screen;
+		set_rect(img_rect_dirty, &bgrect_image);
+		set_rect(dirty_screen, &bgrect_screen);
+#ifdef DDBLT_WAIT
+#define WAITFLAG DDBLT_WAIT
+#else
+#define WAITFLAG DDBLT_WAITNOTBUSY
+#endif
+		bgimage->Blt(&bgrect_image, v->get_surface(), &bgrect_screen, WAITFLAG, NULL);
+		// Then draw the image
+		v->draw(m_ddsurf, img_rect_dirty, img_reg_rc_dirty, FALSE, (dx_transition*)0);
+		// And finally transition in the background bits saved previously
+		v->draw(bgimage, img_rect_dirty, img_reg_rc_dirty, false, tr);
+		bgimage->Release();
+	} else {
+		v->draw(m_ddsurf, img_rect_dirty, img_reg_rc_dirty, FALSE, tr);
+	}
+#endif // 1
 #if 0	
 	dx_window *cwindow = (dx_window *)window;
 	AmbulantView *view = (AmbulantView *)cwindow->view();
