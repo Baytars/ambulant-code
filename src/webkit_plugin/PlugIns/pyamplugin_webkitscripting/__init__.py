63cc0bac8547cbb2f6718c7599441bcf2d41a368
Jack Jansen <Jack.Jansen@cwi.nl>
2007-10-28 22:30:08 +0000

    Infrastructure is complete (but contains a number of serious hacks): the WebPluginContainer object from Safari (which has references to the HTML document and anything else we need) is successfully communicated to the Python code through Ambulant, and can be used from there (right now it shows a message in the status line).

--- src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
+++ src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
@@ -4,9 +4,17 @@ print sys.path
 import ambulant
 print "STEP TWO - Ambulant version is", ambulant.get_version()
 
+embedder = None
+def set_extra_data(idd):
+	global embedder
+	embedder = idd
+	
 def initialize(*args):
     print "STEP THREE - Initialize called"
     print "ARGS", args
     import objc
     import WebKit
     print "STEP FOUR - pyobjc objc and WebKit imported!"
+    print "embedder is", embedder
+    embedder.webPlugInContainerShowStatus_("AmbulantWebKitPlugin: glue loaded")
+	
