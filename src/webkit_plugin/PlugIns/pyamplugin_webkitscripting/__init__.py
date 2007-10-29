900d862fea18532c1fe7ce33676a412f7274888d
Jack Jansen <Jack.Jansen@cwi.nl>
2007-10-29 11:17:46 +0000

    Boilerplate code for state implementation. It loads into Ambulant and it is called correctly (but it doesn't do anything yet).

--- src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
+++ src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
@@ -1,36 +1,48 @@
 print "STEP ONE - Python code loaded into python plugin in ambulant in Safari"
 import sys
-print sys.path
+##print sys.path
 import ambulant
 print "STEP TWO - Ambulant version is", ambulant.get_version()
-
+import pyamplugin_webkitscripting.state
 embedder = None
 def set_extra_data(idd):
 	global embedder
 	embedder = idd
 	
-def initialize(*args):
-    print "STEP THREE - Initialize called"
-    print "ARGS", args
-    import objc
-    import WebKit
-    print "STEP FOUR - pyobjc objc and WebKit imported!"
-    print "WebPlugInContainer is", embedder
+keeper_hack = []
+
+def initialize(apiversion, factories, gui_player):
+    print 'pyamplugin_webkitscripting: initialize() called'
+    if not gui_player:
+    	# This is the initial initialize call, before a document
+    	# is opened. Ignore, we'll get another one later.
+    	return
+
+    keeper_hack.append(factories)
+    keeper_hack.append(gui_player)
+    
+    print "pyamplugin_webkitscripting: WebPlugInContainer is", embedder
     embedder.webPlugInContainerShowStatus_("AmbulantWebKitPlugin: glue loaded")
     webframe = embedder.webFrame()
-    print "WebFrame is", webframe
+    print "pyamplugin_webkitscripting: WebFrame is", webframe
     domdocument = webframe.DOMDocument()
-    print "DOMDocument is", domdocument
-    print "Contents:", dir(domdocument)
-    statenode = domdocument.getElementById_("smilstate")
-    print "smilstate element is", statenode
-    firstlist = statenode.getElementsByTagName_("varone")
-    first = firstlist.item_(0)
-    print "first variable is", first
-    firstvalue = first.firstChild()
-    print "first variable value is", firstvalue
-    print "value was", firstvalue.nodeValue()
-    firstvalue.setNodeValue_("Een")
-    print "after setting, it is", firstvalue.nodeValue()
-    
+
+    sf = pyamplugin_webkitscripting.state.MyStateComponentFactory(domdocument)
+    gsf = factories.get_state_component_factory()
+    if not gsf:
+        gsf = ambulant.get_global_state_component_factory()
+        factories.set_state_component_factory(gsf)
+    gsf.add_factory(sf)
+
+##    statenode = domdocument.getElementById_("smilstate")
+##    print "smilstate element is", statenode
+##    firstlist = statenode.getElementsByTagName_("varone")
+##    first = firstlist.item_(0)
+##    print "first variable is", first
+##    firstvalue = first.firstChild()
+##    print "first variable value is", firstvalue
+##    print "value was", firstvalue.nodeValue()
+##    firstvalue.setNodeValue_("Een")
+##    print "after setting, it is", firstvalue.nodeValue()
+##    
     
