b98416af0ab131813895e0aad130d7b17c9efdb9
Jack Jansen <Jack.Jansen@cwi.nl>
2007-10-29 10:24:19 +0000

    Another milestone: successfully modified the HTML document from the Python code in the ambulant plugin.

--- src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
+++ src/webkit_plugin/PlugIns/pyamplugin_webkitscripting/__init__.py
@@ -21,6 +21,16 @@ def initialize(*args):
     print "WebFrame is", webframe
     domdocument = webframe.DOMDocument()
     print "DOMDocument is", domdocument
+    print "Contents:", dir(domdocument)
     statenode = domdocument.getElementById_("smilstate")
     print "smilstate element is", statenode
+    firstlist = statenode.getElementsByTagName_("varone")
+    first = firstlist.item_(0)
+    print "first variable is", first
+    firstvalue = first.firstChild()
+    print "first variable value is", firstvalue
+    print "value was", firstvalue.nodeValue()
+    firstvalue.setNodeValue_("Een")
+    print "after setting, it is", firstvalue.nodeValue()
+    
     
