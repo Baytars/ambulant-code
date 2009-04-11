48d3113434ecc24dc6e63756f81323e1c5d7efe2
Kees Blom <Kees.Blom@cwi.nl>
2009-04-11 23:45:28 +0000

    Test script for our distributed media now runs on Linux and Cygwin.

--- Documentation/misc/Run-tests.sh
+++ Documentation/misc/Run-tests.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
-set -x # uncomment for debug
+# set -x # uncomment for debug
+AMBULANT_PLAYER=AmbulantPlayer 
 if [ $OSTYPE = linux-gnu ]
 then AMBULANT_EXTRAS=$HOME/share/ambulant
 elif  [ $OSTYPE = cygwin ]
 then AMBULANT_EXTRAS=/cygdrive/c/Program\ Files/AmbulantPlayer-2.0/Extras
+	[3~[3~[3~[3~[3~AMBULANT_PLAYER=/cygwin/c/Program Files[D[D[D[D[D[D\ Files/AmbulantPlayer-2.0/AmbulantPlayer.exe
 fi
 
-AMBULANTPLAYER=AmbulantPlayer 
+AMBULANT_PLAYER=AmbulantPlayer 
 
+# test all sepcified files/urls 
 testall() {
        for i in $* ; do
-		   if [ ! -e $i ]
-		   then return
-		   fi
 		   echo $i
-		   $AMBULANTPLAYER $i
+		   "$AMBULANT_PLAYER" $i
 	   done
 }
 
+# get specified zip file from web, unpack and test all contained .smil files
 testzip() {
 	dir=`basename $1 .zip` 
 	echo wget:  $1 $dir
@@ -33,29 +34,29 @@ testzip() {
 }
 
 
-testall `ls -b "$AMBULANT_EXTRAS"/Welcome/*.smil`
-testall `ls -b "$AMBULANT_EXTRAS"/DemoPresentation/*.smil`
+(cd "$AMBULANT_EXTRAS/Welcome";testall *.smil)
+(cd "$AMBULANT_EXTRAS/DemoPresentation";testall *.smil)
 
-testzip http://ambulantplayer.org/Demos/smilText.zip
-testzip http://ambulantplayer.org/Demos/PanZoom.zip
-testzip http://ambulantplayer.org/Demos/VideoTests.zip
-testzip http://ambulantplayer.org/Demos/Birthday.zip
-testzip http://ambulantplayer.org/Demos/Euros.zip
-testzip http://ambulantplayer.org/Demos/Flashlight.zip
-testzip http://ambulantplayer.org/Demos/News.zip
+testzip http://www.ambulantplayer.org/Demos/smilText.zip
+testzip http://www.ambulantplayer.org/Demos/PanZoom.zip
+testzip http://www.ambulantplayer.org/Demos/VideoTests.zip
+testzip http://www.ambulantplayer.org/Demos/Birthday.zip
+testzip http://www.ambulantplayer.org/Demos/Euros.zip
+testzip http://www.ambulantplayer.org/Demos/Flashlight.zip
+testzip http://www.ambulantplayer.org/Demos/News.zip
 
-testall http://ambulantplayer.org/Demos/smilText/NYC-sT.smil
-testall http://ambulantplayer.org/Demos/smilText/NYC-sT-rtsp.smil
-testall http://ambulantplayer.org/Demos/PanZoom/Fruits-4s.smil
-testall http://ambulantplayer.org/Demos/VideoTests/VideoTests.smil
-testall http://ambulantplayer.org/Demos/VideoTests/VideoTests-rtsp.smil
-testall http://ambulantplayer.org/Demos/Birthday/HappyBirthday.smil
-testall http://ambulantplayer.org/Demos/Birthday/HappyBirthday-rtsp.smil
-testall http://ambulantplayer.org/Demos/Euros/Euros.smil
-testall http://ambulantplayer.org/Demos/Euros/Euros-rtsp.smil 
-testall http://ambulantplayer.org/Demos/Flashlight/Flashlight-US.smil
-testall http://ambulantplayer.org/Demos/Flashlight/Flashlight-US-rtsp.smil
-testall http://ambulantplayer.org/Demos/News/DanesV2-Desktop.smil
-testall http://ambulantplayer.org/Demos/News/DanesV2-Desktop-rtsp.smil
+testall http://www.ambulantplayer.org/Demos/smilText/NYC-sT.smil
+testall http://www.ambulantplayer.org/Demos/smilText/NYC-sT-rtsp.smil
+testall http://www.ambulantplayer.org/Demos/PanZoom/Fruits-4s.smil
+testall http://www.ambulantplayer.org/Demos/VideoTests/VideoTests.smil
+testall http://www.ambulantplayer.org/Demos/VideoTests/VideoTests-rtsp.smil
+testall http://www.ambulantplayer.org/Demos/Birthday/HappyBirthday.smil
+testall http://www.ambulantplayer.org/Demos/Birthday/HappyBirthday-rtsp.smil
+testall http://www.ambulantplayer.org/Demos/Euros/Euros.smil
+testall http://www.ambulantplayer.org/Demos/Euros/Euros-rtsp.smil 
+testall http://www.ambulantplayer.org/Demos/Flashlight/Flashlight-US.smil
+testall http://www.ambulantplayer.org/Demos/Flashlight/Flashlight-US-rtsp.smil
+testall http://www.ambulantplayer.org/Demos/News/DanesV2-Desktop.smil
+testall http://www.ambulantplayer.org/Demos/News/DanesV2-Desktop-rtsp.smil
 
 echo $0 " finished"
