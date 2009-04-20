0d7f5124645c11f90b99b21ca8b6394dd487587d
Jack Jansen <Jack.Jansen@cwi.nl>
2009-04-20 12:34:47 +0000

    Partial adaptation of run-tests for Mac.

--- Documentation/misc/Run-tests.sh
+++ Documentation/misc/Run-tests.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
 # set -x # uncomment for debug
-AMBULANT_PLAYER=AmbulantPlayer 
-if [ $OSTYPE = linux-gnu ]
-then AMBULANT_EXTRAS=$HOME/share/ambulant
-elif  [ $OSTYPE = cygwin ]
-then AMBULANT_EXTRAS=/cygdrive/c/Program\ Files/AmbulantPlayer-2.0/Extras
-	[3~[3~[3~[3~[3~AMBULANT_PLAYER=/cygwin/c/Program Files[D[D[D[D[D[D\ Files/AmbulantPlayer-2.0/AmbulantPlayer.exe
+
+# By default, use wget to get URLs
+wgetorcurl() {
+    wget $1
+}
+
+case "$OSTYPE" in
+linux-gnu)
+    AMBULANT_PLAYER=AmbulantPlayer 
+    AMBULANT_EXTRAS=$HOME/share/ambulant
+    ;;
+cygwin)
+    AMBULANT_EXTRAS=/cygdrive/c/Program\ Files/AmbulantPlayer-2.0/Extras
+	AMBULANT_PLAYER=/cygwin/c/Program\ Files/AmbulantPlayer-2.0/AmbulantPlayer.exe
+	;;
+darwin*)
+    AMBULANT_DEMOS=/Volumes/Ambulant-2.0.2-mac/DemoPresentation
+    AMBULANT_PLAYER=/Applications/Ambulant\ Player.app/Contents/MacOS/Ambulant-mac
+    AMBULANT_WELCOME=/Applications/Ambulant\ Player.app/Contents/Resources/Welcome.smil
+    
+    wgetorcurl() {
+        curl -o $2 $1
+    }   
+    ;;
+*)
+    echo Unsupported platform \"$OSTYPE\", please edit Run-tests.sh
+    exit 1
+    ;;
+esac 
+
+if test ! -f "$AMBULANT_PLAYER" ; then
+    echo Cannot find ambulant player in $AMBULANT_PLAYER
+    echo Edit Run-tests.sh
+    exit 1
 fi
 
-AMBULANT_PLAYER=AmbulantPlayer 
-
+#
 # test all sepcified files/urls 
 testall() {
        for i in $* ; do
@@ -20,9 +47,9 @@ testall() {
 
 # get specified zip file from web, unpack and test all contained .smil files
 testzip() {
-	dir=`basename $1 .zip` 
+	dir=`basename $1 .zip`
 	echo wget:  $1 $dir
-	wget $1
+	wgetorcurl $1 $dir.zip
 	zipfile=$dir.zip
 	mkdir -p $dir
 	cd $dir
@@ -34,8 +61,17 @@ testzip() {
 }
 
 
-(cd "$AMBULANT_EXTRAS/Welcome";testall *.smil)
-(cd "$AMBULANT_EXTRAS/DemoPresentation";testall *.smil)
+if test -d "$AMBULANT_EXTRAS" ; then
+    (cd "$AMBULANT_EXTRAS/Welcome";testall *.smil)
+    (cd "$AMBULANT_EXTRAS/DemoPresentation";testall *.smil)
+elif test -d "$AMBULANT_DEMOS" ; then
+    for i in "$AMBULANT_DEMOS"/*.smil
+    do
+        testall $i
+    done
+else
+    echo Warning: Cannot find DemoPresentation, edit Run-tests.sh
+fi
 
 testzip http://www.ambulantplayer.org/Demos/smilText.zip
 testzip http://www.ambulantplayer.org/Demos/PanZoom.zip
