67f6bc788634f1aea757195949ac7e55c3362264
Bo Gao <Bo.Gao@cwi.nl>
2009-07-10 12:42:23 +0000

    Seamless for RTSP begins to work; However, Prefetch for RTSP works for 75%, it means in sptest-07-av.smil, only the first three segments work well, the last one doesn't work.

--- src/libambulant/net/demux_datasource.cpp
+++ src/libambulant/net/demux_datasource.cpp
@@ -204,7 +204,7 @@ demux_audio_datasource::push_data(timestamp_t pts, const uint8_t *inbuf, int sz)
 	bool rv = true;
 	m_lock.enter();
 	m_src_end_of_file = (sz == 0);
-	AM_DBG lib::logger::get_logger()->debug("demux_audio_datasource.push_data: %d bytes available (ts = %lld)", sz, pts);
+	/*AM_DBG*/ lib::logger::get_logger()->debug("demux_audio_datasource.push_data: %d bytes available (ts = %lld)", sz, pts);
 	if ( ! m_src_end_of_file) {
 		if (_buffer_full()) {
 			rv = false;
@@ -585,7 +585,7 @@ demux_video_datasource::push_data(timestamp_t pts, const uint8_t *inbuf, int sz)
 		return false;
 	}
 	m_src_end_of_file = (sz == 0);
-	AM_DBG lib::logger::get_logger()->debug("demux_video_datasource::push_data(): receiving data sz=%d ,pts=%lld", sz, pts);
+	/*AM_DBG*/ lib::logger::get_logger()->debug("demux_video_datasource::push_data(): receiving data sz=%d ,pts=%lld", sz, pts);
 	if(sz > 0) {
 		//m_frame_nr++;
 		//write_data(m_frame_nr, (char*) inbuf, sz);
