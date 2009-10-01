1485cd651cfaf80fd7420e9fdafa0fd5c50c2732
Jack Jansen <Jack.Jansen@cwi.nl>
2009-10-01 22:10:06 +0000

    Getting to build for OSX 10.4.

--- src/player_macosx/internalize_dylibs.py
+++ src/player_macosx/internalize_dylibs.py
@@ -2,13 +2,38 @@
 # Examine a framework and recursively search for external
 # dynamic libraries. Copy all of these into the framework.
 #
+import sys
+import os
+import shutil
+import subprocess
+import re
+
+OTOOL_MATCHER=re.compile(r'(.*) \(compatibility version .*, current version .*\)')
+
+MACOSX_BUNDLE_DIRS = (
+	'Contents/MacOS',
+	'Contents/Frameworks',
+	'Contents/PlugIns',
+	[
+		'/System/',
+		'/lib/',
+		'/usr/lib/',
+		'@loader_path/',
+		'@executable_path/',
+	]
+)
+
+
+# XXX Needs work for iPhone (because of different relative paths)
 
 class Internalizer:
-	def __init__(self, bundle):
+	def __init__(self, bundle, (run_dir, framework_dir, plugin_dir, safe_prefixes)):
 		self.bundle_dir = bundle
-		self.run_dir = os.path.join(bundle, 'Contents/MacOS')
-		self.framework_dir = os.path.join(bundle, 'Contents/Frameworks')
-		self.plugin_dir = os.path.join(bundle, 'Contents/PlugIns')
+		self.run_dir = os.path.join(bundle, run_dir)
+		self.framework_dir = os.path.join(bundle, framework_dir)
+		self.plugin_dir = os.path.join(bundle, plugin_dir)
+		self.safe_prefixes = safe_prefixes
+		self.safe_prefixes.append(bundle) # XXX good idea?
 		
 		self.todo = {}
 		self.done = {}
@@ -27,15 +52,21 @@ class Internalizer:
 				name = os.path.join(dirpath, name)
 				self.add(name)
 		
+		self.norun = False
+		
 	def add(self, src, copy=False):
+		while os.path.islink(src):
+			src = os.path.realpath(src)
 		if src in self.todo or src in self.done:
 			return
 		if not self.is_loadable(src):
 			return
+		print '* add', src
 		if copy:
 			dstname = os.path.basename(src)
 			if dstname in self.used:
-				assert 0
+				print '** destname', dstname, 'in use'
+				return
 			self.used[dstname] = True
 		else:
 			dstname = None
@@ -43,7 +74,7 @@ class Internalizer:
 		
 	def run(self):
 		while self.todo:
-			src, dst = self.todo.items[0]
+			src, dst = self.todo.items()[0]
 			del self.todo[src]
 			self.process(src, dst)
 			self.done[src] = dst
@@ -55,20 +86,69 @@ class Internalizer:
 			if self.must_copy(lib):
 				self.add(lib, copy=True)
 				must_change.append(lib)
-		self.copy(src, dst)
-		self.set_name(dst)
+		if dst:
+			self.copy(src, dst)
+			self.set_name(dst)
 		for lib in must_change:
 			self.modify_reference(dst, lib)
 		
 	def copy(self, src, dst):
+		if '.framework/' in src:
+			print '** Warning: About to copy from framework:', src
 		dstfilename = os.path.join(self.framework_dir, dst)
-		shutil.copy(src, dstfilename)
+		if self.norun:
+			print 'copy', src, dstfilename
+		else:
+			shutil.copy(src, dstfilename)
 		
 	def get_libs(self, src):
-		assert 0
+		proc = subprocess.Popen(['otool', '-L', src],
+			stdout=subprocess.PIPE)
+		rv = []
+		for line in proc.stdout.readlines():
+			line = line.strip()
+			matches = OTOOL_MATCHER.match(line)
+			if matches:
+				rv.append(matches.group(1))
+		return rv
 		
 	def set_name(self, dst):
 		dstfilename = os.path.join(self.framework_dir, dst)
 		dstfileid = os.path.join("@loader_path/../Frameworks/", dst)
+		if self.norun:
+			print 'setname', dstfilename, dstfileid
+		else:
+			assert 0
+			
+	def modify_reference(self, dst, lib):
+		libid = os.path.join("@loader_path/../Frameworks/", lib)
+		if self.norun:
+			print 'modify-lib', dst, lib, libid
+			
+	def must_copy(self, lib):
+		for prefix in self.safe_prefixes:
+			if  os.path.commonprefix([prefix, lib]) == prefix:
+				return False
+		return True
 		
+	def is_loadable(self, file):
+		proc = subprocess.Popen(['file', file], 
+				stdout=subprocess.PIPE,
+				stderr=open('/dev/null', 'w'))
+		line = proc.stdout.readline()
+		rv = 'Mach-O' in line
+		sts = proc.wait()
+		return rv
 		
+def main():
+	if len(sys.argv) != 2:
+		print 'Usage: %s bundlepath '% sys.argv[0]
+		print 'Recursively slurp dylibs used in a bundle.'
+		sys.exit(1)
+	internalizer = Internalizer(os.path.realpath(sys.argv[1]), MACOSX_BUNDLE_DIRS)
+	internalizer.norun = True
+	internalizer.run()
+		
+if __name__ == '__main__':
+	main()
+	
