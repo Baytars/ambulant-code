4d2d2787863696471539888ae85115772ebcd925
Jack Jansen <Jack.Jansen@cwi.nl>
2009-12-18 10:17:06 +0000

    Found a lock inversion: dx_window::m_redraw_lock is sometimes acquired before acquiring event_processor::m_lock, and sometimes it is the other way around.
    Should fix #2916601.

--- src/libambulant/lib/event_processor.cpp
+++ src/libambulant/lib/event_processor.cpp
@@ -160,7 +160,11 @@ event_processor_impl::cancel_all_events()
 void 
 event_processor_impl::_serve_events()
 {
-	if (m_observer) m_observer->lock_redraw();
+	if (m_observer) {
+		m_lock.leave();
+		m_observer->lock_redraw();
+		m_lock.enter();
+	}
 	// check all delta_timer queues, in the right order
 	while (_events_available(m_high_delta_timer, &m_high_q)
 		|| _events_available(m_med_delta_timer, &m_med_q)
@@ -202,7 +206,11 @@ event_processor_impl::_serve_events()
 #endif
         
             
-	if (m_observer) m_observer->unlock_redraw();
+	if (m_observer) {
+		m_lock.leave();
+		m_observer->unlock_redraw();
+		m_lock.enter();
+	}
 }
 
 bool
