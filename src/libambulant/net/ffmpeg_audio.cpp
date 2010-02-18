343cc24678702dd90703adeb155b6ded7169e2cf
Bo Gao <Bo.Gao@cwi.nl>
2010-02-18 09:51:56 +0000

    Fixed the chopping audio playback in vobis/ogg For some reason which I don't understand, In the current version of ffmpeg, for reading vorbis in ogg, sometime, the pts and dts got by ffmpeg is not valid any more (which equal to AV_NOPTS_VALUE) and this kind of invalid value of pts and dts will last in the following packets for some small while. This invalid pts and dts will cause ffmpeg_decoder_datasource.data_avail dropping packets which should not be dropped. This kind of dropping will cause the chopping audio effect when playback vorbis in ogg. In this case, we use the latest valid pts as the current pts instead of the invalid AV_NOPTS_VALUE.

--- src/libambulant/net/ffmpeg_audio.cpp
+++ src/libambulant/net/ffmpeg_audio.cpp
@@ -427,7 +427,13 @@ ffmpeg_decoder_datasource::data_avail()
 					// So we request 15 bytes more, pass an aligned pointer, and copy down if needed.
 					short *ffmpeg_outbuf = (short *)(((size_t)outbuf+FFMPEG_OUTPUT_ALIGNMENT-1) & ~(FFMPEG_OUTPUT_ALIGNMENT-1));
 					AM_DBG lib::logger::get_logger()->debug("avcodec_decode_audio(0x%x, 0x%x, 0x%x(%d), 0x%x, %d)", (void*)m_con, (void*)outbuf, (void*)&outsize, outsize, (void*)inbuf, cursz);
-					int decoded = avcodec_decode_audio2(m_con, ffmpeg_outbuf, &outsize, inbuf, cursz);
+					/////xxxbo 12-feb-2010: Adapted to the new api avcodec_decode_audio3
+					AVPacket gb_avp;
+					av_init_packet(&gb_avp);
+					gb_avp.data = inbuf;
+					gb_avp.size = cursz;
+					//int decoded = avcodec_decode_audio2(m_con, ffmpeg_outbuf, &outsize, inbuf, cursz);
+					int decoded = avcodec_decode_audio3(m_con, ffmpeg_outbuf, &outsize, &gb_avp);
 					if (decoded < 0) outsize = 0;
 #if FFMPEG_OUTPUT_ALIGNMENT-1
 					if (outsize > 0 && (uint8_t *)ffmpeg_outbuf != outbuf) 
@@ -446,7 +452,8 @@ ffmpeg_decoder_datasource::data_avail()
 						outsize = AVCODEC_MAX_AUDIO_FRAME_SIZE;
 						outbuf = (uint8_t*) m_buffer.get_write_ptr(outsize);
 						ffmpeg_outbuf = (short *)(((size_t)outbuf+FFMPEG_OUTPUT_ALIGNMENT-1) & ~(FFMPEG_OUTPUT_ALIGNMENT-1));
-						decoded = avcodec_decode_audio2(m_con, (short*) ffmpeg_outbuf, &outsize, inbuf, cursz);
+						//decoded = avcodec_decode_audio2(m_con, (short*) ffmpeg_outbuf, &outsize, inbuf, cursz);
+						decoded = avcodec_decode_audio3(m_con, (short*) ffmpeg_outbuf, &outsize, &gb_avp);
 						if (decoded < 0) outsize = 0;
 						AM_DBG lib::logger::get_logger()->debug("avocodec_decode_audio: converted additional %d of %d bytes to %d", decoded, cursz, outsize);
 #if FFMPEG_OUTPUT_ALIGNMENT-1
@@ -494,10 +501,13 @@ ffmpeg_decoder_datasource::data_avail()
 							assert(m_buffer.size() > bytes_unwanted);
 							m_buffer.readdone(bytes_unwanted);
 						}
-					} else {
+					}
+					
+					else {
 						AM_DBG lib::logger::get_logger()->debug("ffmpeg_decoder_datasource.data_avail: m_elapsed = %lld < clip_begin = %lld, skipped %d bytes", m_elapsed, m_src->get_clip_begin(), outsize);
 						m_buffer.pushdata(0);
 					}
+					
 
 					AM_DBG lib::logger::get_logger()->debug("ffmpeg_decoder_datasource.data_avail : m_src->readdone(%d) called m_src=0x%x, this=0x%x", decoded,(void*) m_src, (void*) this );
 				} else {
