ee44cc7f217acfb5ea0a522c0e531a44ce8d28c1
Kees Blom <Kees.Blom@cwi.nl>
2010-03-08 13:48:43 +0000

    Disabled code to skip bytes in a buffer to perform a seek request. Enable http_seek_workaround() by adlusting FFMPEG_HTTP_SEEK_BUG_MAX_VERSION to ffmpeg-export-2010-01-22 version we now use.

--- src/libambulant/net/ffmpeg_audio.cpp
+++ src/libambulant/net/ffmpeg_audio.cpp
@@ -626,22 +626,22 @@ ffmpeg_decoder_datasource::seek(timestamp_t time)
 	m_lock.enter();
     bool skip_seek = false;
     assert( time >= 0);
+	/* Jack requested to disable the code to perform the seek by skipping byes in the buffer for #2954199
 	int nbytes = m_buffer.size();
     AM_DBG lib::logger::get_logger()->debug("ffmpeg_decoder_datasource(0x%x)::seek(%ld), discard %d bytes, old time was %ld", (void*)this, (long)time, nbytes, m_elapsed);
 	if (nbytes) {
         timestamp_t buffer_begin_elapsed = m_elapsed - 1000000LL * (m_buffer.size() * 8) / (m_fmt.samplerate* m_fmt.channels * m_fmt.bits);
         // If the requested seek time falls within the buffer we are in luck, and do the seek by dropping some data.
-		// Jack suggests to disable next if stnt because of #2954199, but this does not work
         if (time >= buffer_begin_elapsed && time < m_elapsed) {
             nbytes = ((time-buffer_begin_elapsed) * (m_fmt.samplerate* m_fmt.channels * m_fmt.bits)) / (8LL * 1000000LL);
 			nbytes &= ~0x1; //  nbytes may be odd s.t. resulting pointer becomes unuseable for ffmpeg; fixes #2954199
             skip_seek = true;
         }
-		/* end of disabled code for #2954199 */
         AM_DBG lib::logger::get_logger()->debug("ffmpeg_decoder_datasource: flush buffer (%d bytes) due to seek", nbytes);
 		(void)m_buffer.get_read_ptr();
 		m_buffer.readdone(nbytes);
 	}
+	/ * end of disabled code for #2954199 */
     if (!skip_seek) {
         m_src->seek(time);
         m_elapsed = time; // XXXJACK not needed??
