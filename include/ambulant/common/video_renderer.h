bd87bdd45e26b39a082451aec049812c5d337dc1
Kees Blom <Kees.Blom@cwi.nl>
2013-02-21 11:55:08 +0100

    Added more packet tracing, from av_read() until SDL redraw()

--- include/ambulant/common/video_renderer.h
+++ include/ambulant/common/video_renderer.h
@@ -93,6 +93,7 @@ class video_renderer : public common::renderer_playable {
 	/// eventually free()ing frame. This method is protected because it shares
 	/// the m_lock mutex.
 	virtual void _push_frame(char* frame, size_t size) = 0;
+	virtual void _push_frame(char* frame, size_t size, unsigned long int ts) { _push_frame (frame, size); } // PKT_TRACE
 
   protected:
 	lib::size m_size;		///< (width, height) of the video data.
